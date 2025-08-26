a94e91f44b77b084593fb957c23e62e2548f803f
Jeffrey D Varner <jdv27@cornell.edu>
2025-08-26 14:17:01 -0400

    Add lab solutions and lecture notebooks for weeks 1 and 2
    Added CHEME-5800-L1d-Solution-FunWithHexadecimalNumbers-Fall-2025.ipynb and supporting files (Include.jl, Project.toml) for week 1, and CHEME-5800-L2b-Lecture-IntroductionToFunctions-Fall-2025.ipynb for week 2. These files introduce base 16 numbers, ASCII/Unicode encoding, and function basics in Julia, supporting the CHEME 5800 course material.

---
+++ labs/week-1/L1d/Include.jl
@@ -0,0 +1,28 @@
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# check do we have a Manifest.toml file?
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages
+using VLDataScienceMachineLearningPackage
+using CSV
+using DataFrames
+using FileIO
+using StatsPlots
+using Plots
+using Colors
+using Statistics
+using LinearAlgebra
+using Distances
+using JLD2
+using PrettyTables
+using Clustering
+using Distributions
+using Unicode
+using Test
