9e50af19ff0159b6118b53b7cc1b72608682fe44
Jeffrey Varner <jdv27@cornell.edu>
2025-10-26 11:07:59 -0400

    Revise Weighted Majority Algorithm lab and update code
    Improved the instructional content and learning objectives in the Weighted Majority Algorithm lab notebook, clarified expert and adversary function implementations, and updated code cells for data loading, expert selection, and simulation. Renamed Include.jl to Include-solution.jl, updated Project.toml, and made minor corrections to the SVD SP500 example notebook.

--- labs/week-10/L10b/Include-solution.jl
+++ labs/week-10/L10b/Include-solution.jl
@@ -23,11 +23,3 @@ using DataFrames
 using CSV
 using FileIO
 using KernelFunctions
-
-# load my codes -
-include(joinpath(_PATH_TO_SRC, "Types.jl"));
-include(joinpath(_PATH_TO_SRC, "Factory.jl"));
-include(joinpath(_PATH_TO_SRC, "Files.jl"));
-include(joinpath(_PATH_TO_SRC, "Compute.jl"));
-include(joinpath(_PATH_TO_SRC, "Online.jl"));
-
