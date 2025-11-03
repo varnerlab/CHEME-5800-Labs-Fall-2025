44e579535d3b101a129f267c256c5b38f052343e
Jeffrey Varner <jdv27@cornell.edu>
2025-11-03 12:03:05 -0500

    Add Markov word generator lab notebooks and setup files
    Added solution and student versions of the Markov word generator lab notebooks for CHEME-5800 Fall 2025, along with supporting Julia include files and a Project.toml for environment setup. These materials guide students through building a character-level Markov model for word generation using a large English vocabulary dataset.

---
+++ labs/week-11/L11b/Include-student.jl
@@ -0,0 +1,20 @@
+# setup paths -
+const _ROOT = @__DIR__
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# check: do we need to download any packages?
+using Pkg
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load the required packages -
+# using VLDataScienceMachineLearningPackage # did you catch this bug? (we reply this package, so we need to load with a using statement)
+using Distributions
+using Plots
+using Colors
+using LinearAlgebra
+using Statistics
+using DataFrames
+using PrettyTables
